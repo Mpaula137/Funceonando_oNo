6d9d47e44e6e3c766b31c9f0e6abe3b1078f8e3e
Maria Paula <124606922+Mpaula137@users.noreply.github.com>
2023-03-20 16:14:10 -0500

    Add files via upload

---
+++ vueltas.py
@@ -0,0 +1,16 @@
+def calcular_las_vueltas (P:int, M:int, H:int, B:float) :# ((cant) P: panes, M:bolsas de leche, H: huevos, B: cantidad de dinero )
+    Preciototal = ((P*300)+(M*3300)+(H*350)) # (precio de pan, bolsa de leche y huevos)
+    vueltas= B - Preciototal
+    return vueltas
+if __name__ == "__main__":  #Funcion principal
+    P= int(input("ingrese la cantidad de panes:"))
+    M= int(input("ingrese la cantidad de bolsa (s) de leche:"))
+    H= int(input("ingrese la cantidad de huevos:"))
+    B=float (input("cantidad de dinero con el que va a pagar:"))
+    total_vueltas= calcular_las_vueltas (P, M, H, B)
+if calcular_las_vueltas < 0 :
+    print ("Te falto"+ str(total_vueltas))
+else:
+    print ("El total a devolver es:"+ str(total_vueltas))
+
+
